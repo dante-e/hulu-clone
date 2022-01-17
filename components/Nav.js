86a36de11c52c20dbb9ee97c472f7f937626ada5
dante-e <55149262+dante-e@users.noreply.github.com>
2022-01-16 23:50:44 -0500

    header and nav bar finished

---
+++ components/Nav.js
@@ -0,0 +1,16 @@
+import requests from "../utils/requests"
+
+function Nav() {
+  return (
+    <nav className='relative'>
+    <div className='flex px-10 sm:scroll-px-20 text-2xl whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll scrollbar-hide'>
+      {Object.entries(requests).map(([key, { title, url }]) => (
+        <h2 key={key} className ='last:pr-24 cursor-pointer transition duration-100 transform hover:scale-125 hover:text-white active:text-green-500'>{title}</h2>
+      ) )}
+    </div>
+    <div className='absolute top-0 right-0 bg bg-gradient-to-l from-[#0c313f] to-transparent h-10 w-1/5' />
+    </nav>
+  )
+}
+
+export default Nav
